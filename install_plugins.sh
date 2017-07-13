a982c6eb81afda6779a2af29da9d80ee95163568
Adriano Baptistella <adrianojosebaptistella@gmail.com>
2017-07-13 13:43:20 -0300

    Create install_plugins.sh

---
+++ install_plugins.sh
@@ -0,0 +1,51 @@
+#echo "Atualizar/Upgrade do SO"
+#apt-get update && sudo apt-get upgrade
+
+#echo "Instalando o Build-Essential"
+#apt-get install build-essential
+
+#echo "Instalando o libstdc++6"
+#apt-get install libstdc++6
+
+cd plugins
+
+echo "Progressive Projects"
+git clone https://github.com/stgeneral/redmine-progressive-projects-list.git progressive_projects_list
+
+echo "Editor de Texto"
+git clone https://github.com/a-ono/redmine_ckeditor.git redmine_ckeditor
+
+echo "Issue Badge"
+git clone https://github.com/akiko-pusu/redmine_issue_badge.git redmine_issue_badge
+
+echo "Issue Favicon"
+hg clone https://bitbucket.org/akiko_pusu/redmine_issue_favicon 
+
+echo "Redmine Issue Templates"
+git clone https://github.com/akiko-pusu/redmine_issue_templates.git redmine_issue_templates
+
+echo "Redmine LightBox"
+git clone https://github.com/paginagmbh/redmine_lightbox2.git
+
+echo "Spent Time"
+git clone https://github.com/eyp/redmine_spent_time.git
+
+echo "Integração 0800Net e Redmine"
+git clone https://github.com/ebaptistella/integrator0800.git
+
+echo "Best Timer"
+git clone https://github.com/LeviticusMB/bestest_timer
+
+echo "Redmine Login Audit"
+git clone https://github.com/martin-denizet/redmine_login_audit.git
+
+cd ..
+echo "Finalizado a instalação de plugins"
+echo ""
+
+cd ..
+echo "---INICIANDO bundle install test---"
+bundle install --without development test
+
+echo "Executando migrations de todos os plugins"
+bundle exec rake redmine:plugins:migrate RAILS_ENV=production
