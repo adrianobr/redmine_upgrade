6a9e53c7dd9962fcc68b15a95766890ef7c02f8f
Adriano Baptistella <adrianojosebaptistella@gmail.com>
2017-07-28 11:24:25 -0300

    Update install_plugins.sh

--- install_plugins.sh
+++ install_plugins.sh
@@ -56,6 +56,9 @@ git clone https://github.com/bdemirkir/sidebar_hide.git
 echo "Niko-Niko"
 git clone https://github.com/yuuu/redmine_nikoca_re.git
 
+echo "Redmine Tags"
+git clone https://github.com/ixti/redmine_tags.git
+
 echo "Finalizado a instalação de plugins"
 
 cd ..
